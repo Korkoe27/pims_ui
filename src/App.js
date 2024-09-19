22e29bc8f4a4493af2d31322221c353d46acb7e9
Mechanic <dumashieanthony@gmail.com>
2024-09-19 18:02:42 +0100

    fixed: app.js issues. UI rendering working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Sidebar, CaseHistory, PersonalInfo, ConsultationTab} from './components';
+import { CaseHistory, PersonalInfo, ConsultationTab} from './components';
 import {Dashboard, Appointments, Dispensary, Inventory, Patients,Login} from './pages';
+import Layout from './pages/Layout';
 
 const App = () => {
   return (
-    <div className='overflow-hidden flex'>
+    <div>
       
       <BrowserRouter>
-      <div className='flex flex-row  h-full'>
-        <div className=' bg-white dark:bg-secondary-dark-bg'>
-          <Sidebar />
-        </div>
-
-</div>
         <Routes>
-          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/' element={<Layout/>}>
+            <Route index element={<Dashboard/>} />
+            <Route path='/my-patients' element={<Patients/>}/>
+            <Route path='/appointments' element={<Appointments/>}/>
+            <Route path='/inventory' element={<Inventory/>}/>
+            <Route path='/dispensary' element={<Dispensary/>}/>
+            <Route path='/register-patient' element={<PersonalInfo/>}/>
+          </Route>
           <Route path='/login' element={<Login/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/my-patients' element={<Patients/>}/>
-          <Route path='/appointments' element={<Appointments/>}/>
-          <Route path='/inventory' element={<Inventory/>}/>
-          <Route path='/dispensory' element={<Dispensary/>}/>
-          <Route path='/register-patient' element={<PersonalInfo/>}/>
         </Routes>
+       
       
       </BrowserRouter>
       
