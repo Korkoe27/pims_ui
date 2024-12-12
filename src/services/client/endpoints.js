df22e2dd4048d3a2bf9b73015e8482711c7538fb
Artisan Artisan 🐘🔧 <52928434+Korkoe27@users.noreply.github.com>
2024-12-12 17:12:57 +0000

    feat(api): integrate appointments for dashboard

--- src/services/client/endpoints.js
+++ src/services/client/endpoints.js
@@ -1,3 +1,5 @@
 export  const   loginUrl    =   "auth/api/login/";
 export  const   getDashboardStat    =   "summary/api/dashboard/";
+export  const   getDashboardStat    =   "summary/api/dashboard/";
+export const    listPatients    =   'clients/api/patients/';
 
