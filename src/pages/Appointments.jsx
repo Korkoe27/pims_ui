179a3855ab9f703a9f20451dd0b58fee977dab24
Solomon <sedziah1@gmail.com>
2024-12-16 09:14:06 +0000

    style: remove view button on the today's appointments page

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -42,7 +42,7 @@ const Appointments = () => {
               Status
             </th>
             <th scope="col" className="px-3 min-w-40 py-3">
-              
+              Action
             </th>
           </tr>
         </thead>
@@ -71,7 +71,7 @@ const Appointments = () => {
             
         </td>
         <td className="px-6 py-4 flex gap-10">
-            <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none dark:focus:ring-blue-800">View</button>
+            {/* <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none dark:focus:ring-blue-800">View</button> */}
             <button onClick={() => handleConsult()} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Consult</button>
         </td>
         
