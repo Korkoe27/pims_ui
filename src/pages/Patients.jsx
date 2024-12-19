6248ae7e44c708ee44ae2a6a66ef60967e65d60f
Artisan Artisan 🐘🔧 <52928434+Korkoe27@users.noreply.github.com>
2024-12-19 17:04:45 +0000

    refactor(ui): build header component

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,32 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import { useShowPatients } from "../services/queries/patients-query";
 import LoadingSpinner from "../components/LoadingSpinner";
-import PatientModal from "../components/SelectClinicModal";
-import { GrAdd } from "react-icons/gr";
+import Navbar from "../components/Navbar";
 // import { useAuth } from '../hooks/AuthProvider';
 
 const Patients = () => {
   const { data: patients, isLoading } = useShowPatients();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
   const isTable = true;
   // console.log(JSON.stringify(patients?.data?.results,null,3));
   return (
     <div className="px-8 ml-72 flex flex-col mt-8 gap-8 bg-[#f9fafb] w-full shadow-md sm:rounded-lg">
 
-
-{isModalOpen && <PatientModal setIsModalOpen={setIsModalOpen} />}
+{/* 
+{isModalOpen && <PatientModal setIsModalOpen={setIsModalOpen} />} */}
+    <Navbar />
       <div className="flex justify-between">
-      <h1 className="font-extrabold text-xl">Patients</h1>
-                  <button className='flex items-center p-4 h-14 text-white bg-[#2f3192] gap-2 rounded-md text-sm' 
-                    type='button'
-                    onClick={openModal}
-                  >
-                    <GrAdd/>Add New Patient
-                  </button>
-      </div>
+
+
       {isLoading && <LoadingSpinner isTable={isTable} />}
+      <h1 className="font-extrabold text-xl">Patients</h1>
+
+      </div>
+
      {patients && patients?.data && 
       <table className="w-full text-base text-left rtl:text-right text-gray-500 ">
         <thead className="text-base text-gray-700 uppercase bg-gray-50 ">
