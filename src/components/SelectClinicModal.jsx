3f96c3092585e9856a6f3fa5b5f77471f89b0580
Solomon <sedziah1@gmail.com>
2025-03-03 22:52:43 +0000

    fix: selected clinic modal

--- src/components/SelectClinicModal.jsx
+++ src/components/SelectClinicModal.jsx
@@ -71,7 +71,7 @@ const SelectClinicModal = ({ setIsModalOpen }) => {
             >
               <span
                 className={`border h-6 w-6 rounded-full ${
-                  selectedClinic === "oldsite"
+                  selectedClinic === "Old Site"
                     ? "border-[#2f3192] border-4"
                     : "border-[#1b1c1e]"
                 }`}
@@ -81,15 +81,15 @@ const SelectClinicModal = ({ setIsModalOpen }) => {
             <button
               type="button"
               className={`border group border-[#1b1c1e] text-xl font-bold w-80 h-80 flex flex-col items-end p-6 mx-10 rounded-md cursor-pointer ${
-                selectedClinic === "New Site"
+                selectedClinic === "Science"
                   ? "bg-[#ececf9] border-[#2F3192]"
                   : ""
               }`}
-              onClick={() => handleClinicSelection("New Site")}
+              onClick={() => handleClinicSelection("Science")}
             >
               <span
                 className={`border h-6 w-6 rounded-full ${
-                  selectedClinic === "ucc"
+                  selectedClinic === "Science"
                     ? "border-[#2f3192] border-4"
                     : "border-[#1b1c1e]"
                 }`}
@@ -99,9 +99,7 @@ const SelectClinicModal = ({ setIsModalOpen }) => {
             <button
               type="button"
               className={`border group border-[#1b1c1e] text-xl font-bold w-80 h-80 flex flex-col items-end p-6 mx-10 rounded-md cursor-pointer ${
-                selectedClinic === "CCTU"
-                  ? "bg-[#ececf9] border-[#2F3192]"
-                  : ""
+                selectedClinic === "CCTU" ? "bg-[#ececf9] border-[#2F3192]" : ""
               }`}
               onClick={() => handleClinicSelection("CCTU")}
             >
